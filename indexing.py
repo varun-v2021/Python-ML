48d7e45475d02bb30e9991471f2281bd418fafa2
Varun <varun.v2021@gmail.com>
2020-05-03 00:18:47 +0530

    Add files via upload

--- indexing.py
+++ indexing.py
@@ -2,36 +2,102 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(np.random.randn(8, 4),
-index = ['a','b','c','d','e','f','g','h'], columns = ['A', 'B', 'C', 'D'])
+                  index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], columns=['A', 'B', 'C', 'D'])
 
 print('>>>>>><<<<<<<<<<<<<<')
 training = np.array(df)
 print('training data contents')
 print(training)
 print('-----------all rows of first column----------')
-print(training[:,0])
+print(training[:, 0])
 print('-----------all rows of second column----------')
-print(training[:,1])
+print(training[:, 1])
 
+# list of data
+data = [[11, 22],
+        [33, 44],
+        [55, 66]]
+# array of data
+data = np.array(data)
+print('Rows: %d' % data.shape[0])
+print('Cols: %d' % data.shape[1])
+# Rows: 3
+# Cols: 2
+
+# data_reshaped = data.reshape(data,(3,2,1))
+# print('data_reshaped')
+# print(data_reshaped)
+
+data = np.array([11, 22, 33, 44, 55])
+print(']]]]]]]]]]')
+print(data.shape)
+# 1D to 2D
+data = data.reshape((data.shape[0], 1))
+print(data)
+print(data.shape)
+
+# 2D to 3D
+# The reshape function can be used directly, specifying the new dimensionality.
+# This is clear with an example where each sequence has multiple time steps with
+# one observation (feature) at each time step.
+# We can use the sizes in the shape attribute on the array to specify the number of samples (rows)
+# and columns (time steps) and fix the number of features at 1.
+# shape[0] - rows, shape[1] - columns
+# data.reshape((data.shape[0], data.shape[1], 1))
+
+print('$$$$$$$$$$$$$$$$$$$$$$$')
+data = [[11, 22],
+        [33, 44],
+        [55, 66]]
+# array of data
+data = np.array(data)
+print(data.shape)
+# reshape
+data = data.reshape((data.shape[0], data.shape[1], 1))
+print(data.shape)
+
+print('::::::::::::::::::::::::::::')
+x_data = []
+y_data = []
+# multiple rows having single column
+data = np.array([[11], [22], [33], [44], [55]])
+# above line is equivalent to
+# data = np.array([[11],
+#                [12],
+#                [13],
+#                [14],
+#                [15]])
+for i in range(1, 5):
+    x_data.append(data[i - 1:i, 0])
+    y_data.append(data[i,0])
+print(x_data)
+print('---------')
+print(y_data)
+x_data , y_data = np.array(x_data), np.array(y_data)
+print(',,,,,,,,,,,,,,,')
+print(x_data)
+print(y_data)
+# data = np.reshape(data,(data.shape[0],data.shape[1],1))
+# print(data)
 #
 # .loc()
 #
 # Label based
 
-#select all rows for a specific column
-print (df.loc[:,'A'])
+# select all rows for a specific column
+print(df.loc[:, 'A'])
 print('############')
 # Select all rows for multiple columns, say list[]
-print (df.loc[:,['A','C']])
+print(df.loc[:, ['A', 'C']])
 
 # Select few rows for multiple columns, say list[]
-print (df.loc[['a','b','f','h'],['A','C']])
+print(df.loc[['a', 'b', 'f', 'h'], ['A', 'C']])
 
 # Select range of rows for all columns
-print (df.loc['a':'h'])
+print(df.loc['a':'h'])
 
 # for getting values with a boolean array
-print (df.loc['a']>0) # row 'a' whose column is > 0
+print(df.loc['a'] > 0)  # row 'a' whose column is > 0
 
 print('***************************************************')
 # .iloc()
@@ -41,24 +107,24 @@ print('***************************************************')
 # Pandas provide various methods in order to get purely integer based indexing.
 # Like python and numpy, these are 0-based indexing.
 
-df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A', 'B', 'C', 'D'])
 
 # select all rows for a specific column
-print (df.iloc[:4])
-print (df.iloc[1:9, 2:4]) # since there are only 8 rows of random numbers
+print(df.iloc[:4])
+print(df.iloc[1:9, 2:4])  # since there are only 8 rows of random numbers
 
 # Slicing through list of values
-print (df.iloc[[1, 3, 5], [1, 3]])
-print (df.iloc[1:3, :])
-print (df.iloc[:,1:3])
+print(df.iloc[[1, 3, 5], [1, 3]])
+print(df.iloc[1:3, :])
+print(df.iloc[:, 1:3])
 
 # Let us now see how each operation can be performed on the DataFrame object.
 # We will use the basic indexing operator '[ ]'
 print('=========================================')
-df = pd.DataFrame(np.random.randn(8, 4), columns = ['A', 'B', 'C', 'D'])
-print (df['A'])
-print (df[['A','B']])
-print (df[2:2])
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A', 'B', 'C', 'D'])
+print(df['A'])
+print(df[['A', 'B']])
+print(df[2:2])
 
-#attribute access
-print (df.A)
+# attribute access
+print(df.A)
--- stock_predict.py
+++ stock_predict.py
@@ -15,17 +15,39 @@ from keras.layers import Dense
 # url = 'https://github.com/varun-v2021/ML-SampleData/blob/master/TATAMOTORS-history.csv'
 url = 'https://raw.githubusercontent.com/varun-v2021/ML-SampleData/master/TATAMOTORS-history.csv'
 dataset_train = pd.read_csv(url)
+# get 'Open' column from the dataset into training set
 training_set = dataset_train.iloc[:, 1:2].values
+print('DATASET TO TRAIN')
 print(dataset_train.head())
+print('TRAINING SET')
+print(training_set)
 sc = MinMaxScaler(feature_range=(0,1))
+# training_set_scaled will have multiple rows which have been transformed for values between 0 & 1
+# having single column. (as we have iloc[i, 1:2] reads second column (1) till third column 2 without including
+# '2' column)
 training_set_scaled = sc.fit_transform(training_set)
 
 X_train = []
 Y_train = []
-for i in range(50,2542 ): #60,2035 for tataglobal
+for i in range(50,2542 ): #60,2035 for tataglobal (total no of rows in historic data)
+    # slicing training_set_scaled
+    # training_set_scaled[start_row_index:end_row_index,column index]
+    # X_train will become 1D array of arrays of rows under single column 0
+    # value of array of ([array of 0 to 50 rows], [array 1 to 51 rows], ...)
+    # X_train will contain data in timesteps
     X_train.append(training_set_scaled[i-50:i, 0]) #60
+    # Y_train will become 1D array indexed under single column 0
+    # Y_train will have array of values of 50, 51, 52 ... 2541 of training_set_scaled
     Y_train.append(training_set_scaled[i, 0])
 X_train, Y_train = np.array(X_train), np.array(Y_train)
+# shape[0] gives no of rows and shape[1] gives no of columns
+# 2D to 3D - 3D array is required for LSTM modelling
+# The reshape function can be used directly, specifying the new dimensionality.
+# This is clear with an example where each sequence has multiple time steps with
+# one observation (feature) at each time step.
+# We can use the sizes in the shape attribute on the array to specify the number of samples (rows)
+# and columns (time steps) and fix the number of features at 1.
+# shape[0] - rows, shape[1] - columns
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 model = Sequential()
@@ -53,15 +75,17 @@ pickle.dump(model, open("stock-model.pkl", "wb"))
 # url = 'https://github.com/varun-v2021/ML-SampleData/blob/master/TATAMOTORS-quarterly.csv'
 url = 'https://raw.githubusercontent.com/varun-v2021/ML-SampleData/master/TATAMOTORS-monthly.csv'
 dataset_test = pd.read_csv(url)
+# get 'Open' column from the dataset like the training set
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 50:].values #60
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 X_test = []
-for i in range(50, 90): #60,76
+for i in range(50, 76): #60,76
     X_test.append(inputs[i-50:i, 0]) #60
+
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = model.predict(X_test)
